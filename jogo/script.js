48f6ffe9a5946cf06f4fe30908a8f5546e14e521
Estelita Fernanda <112116442+estelitafernanda@users.noreply.github.com>
2023-11-13 09:46:01 -0300

    Update script.js

--- jogo/script.js
+++ jogo/script.js
@@ -1,66 +1,98 @@
 let canvas = document.getElementById('canvas');
 let context = canvas.getContext('2d');
-let imagem = new Image(); 
-imagem.src ="bonequinha.png"; 
+let imagem = new Image();
+imagem.src = "bonequinha.png";
 
-let x = 0, intervalo = 120;
+let x = 0, intervalo = 120, y = 0, movi = 0;
 
-function bonequinha(){
+document.addEventListener('keydown', function (event) {
+    var key = event.key.toLowerCase();
+
+    if (key === 's') {
+        y += 10;
+        baixo();
+    } else if (key === 'a') {
+        x -= 10;
+        esquerda();
+    } else if (key === 'w') {
+        y -= 10;
+        cima();
+    } else if (key === 'd') {
+        x += 10;
+        direita();
+    }
+});
+
+function baixo() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    if(x===0){
-        context.drawImage(imagem, 15, -30, 100, 100, 5, 0, 160, 160);
+    if (x === 0) {
+        context.drawImage(imagem, 15, -30, 100, 100, x, y, 160, 160);
     }
-    else if(x===1){
-        context.drawImage(imagem, 110, -30, 90, 100, 30, 0, 180, 160);
+    else if (x === 1) {
+        context.drawImage(imagem, 110, -30, 90, 100, x + 30, y, 160, 160);
     }
-    else if(x===2){
-        context.drawImage(imagem, 175, -30, 90, 100, 30, 0, 180, 160);
+    else if (x === 2) {
+        context.drawImage(imagem, 175, -30, 90, 100, x + 40, y, 160, 160);
     }
-    else if(x===3){
-        context.drawImage(imagem, 240, -30, 90, 100, 30, 0, 180, 160);
+    else if (x === 3) {
+        context.drawImage(imagem, 240, -30, 90, 100, x + 50, y, 160, 160);
     }
-   else if(x===4){
-    context.drawImage(imagem, 20, 69, 85, 60, -25, 50, 180, 100);
-   }
-   else if(x===5){
-    context.drawImage(imagem, 95, 70, 75, 65, -20, 57, 180, 100);
-   }
-   else if(x===6){
-    context.drawImage(imagem, 162, 70, 75, 65, -20, 57, 180, 100);
-   } 
-   else if(x===7){
-    context.drawImage(imagem, 228, 70, 75, 65, -20, 57, 180, 100);
-   } 
-   else if(x===8){
-    context.drawImage(imagem, 228, 70, 75, 65, -20, 57, 180, 100);
-   }
-   else if(x===9){ 
-    context.drawImage(imagem, 20, 135, 90, 60, -25, 50, 193, 104);
-   }
-   else if(x===10){ 
-    context.drawImage(imagem, 89, 133, 85, 63, -25, 50, 193, 100);
-   }
-   else if(x===11){
-    context.drawImage(imagem, 155, 133, 85, 63, -25, 49, 193, 102);
-   } 
-   else if(x===12){
-    context.drawImage(imagem, 221, 133, 85, 63, -25, 49, 193, 102);
-   }
-   else if(x===13){
-    context.drawImage(imagem, 24, 199, 85, 63, -25, 49, 197, 104);
-   } 
-   else if(x===14){ 
-    context.drawImage(imagem, 94, 199, 85, 63, -25, 49, 197, 104);
-   }
-   else if(x===15){
-    context.drawImage(imagem, 158, 199, 85, 63, -25, 49, 197, 104);
-   }
-   else if(x===16){
-    context.drawImage(imagem, 224, 199, 85, 63, -25, 49, 197, 104);
-   } 
-   x = (x + 1) % 70;
-   setTimeout (bonequinha, intervalo);
+    movi = (movi + 1) % 4;
+}
+imagem.onload = baixo;
 
+function esquerda() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (x === 0) {
+        context.drawImage(imagem, 20, 69, 85, 60, x, y + 49, 160, 100);
+    }
+    else if (x === 1) {
+        context.drawImage(imagem, 95, 70, 75, 65, x + 10, y + 49, 160, 100);
+    }
+    else if (x === 2) {
+        context.drawImage(imagem, 162, 70, 75, 65, x + 20, y + 49, 160, 100);
+    }
+    else if (x === 3) {
+        context.drawImage(imagem, 228, 70, 75, 65, x + 30, y + 49, 160, 100);
+    }
+    movi = (movi + 1) % 4;
 }
 
-imagem.onload = bonequinha;
+imagem.onload = esquerda;
+
+function direita() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (y === 0) {
+        context.drawImage(imagem, 228, 70, 75, 65, x, y + 49, 160, 100);
+    }
+    else if (y === 1) {
+        context.drawImage(imagem, 20, 135, 90, 60, x + 10, y + 49, 160, 104);
+    }
+    else if (y === 2) {
+        context.drawImage(imagem, 89, 133, 85, 63, x + 20, y + 49, 160, 100);
+    }
+    else if (y === 3) {
+        context.drawImage(imagem, 155, 133, 85, 63, x + 30, y + 49, 160, 102);
+    }
+    movi = (movi + 1) % 4;
+}
+
+imagem.onload = direita;
+function cima() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (x === 0) {
+        context.drawImage(imagem, 24, 199, 85, 63, x, y + 49, 197, 104);
+    }
+    else if (x === 1) {
+        context.drawImage(imagem, 94, 199, 85, 63, x, y + 49, 197, 104);
+    }
+    else if (x === 2) {
+        context.drawImage(imagem, 158, 199, 85, 63, x, y + 49, 197, 104);
+    }
+    else if (x === 3) {
+        context.drawImage(imagem, 224, 199, 85, 63, x, y + 49, 197, 104);
+    }
+    movi = (movi + 1) % 4;
+}
+
+imagem.onload = cima;
