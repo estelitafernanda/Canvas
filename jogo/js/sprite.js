4ea741daa777dcf1904c89bc57e338b5e72cac70
Estelita Fernanda <112116442+estelitafernanda@users.noreply.github.com>
2023-11-14 12:30:51 -0300

    Update sprite.js

--- jogo/js/sprite.js
+++ jogo/js/sprite.js
@@ -1,7 +1,9 @@
- const gravity = 0.6
+ const gravity = 0.2
+
  class Sprite {
-    constructor( {position, dimensions} ){
+    constructor( {position, velocity, dimensions} ){
       this.position = position;
+      this.velocity = velocity;
       this.width = dimensions.width;
       this.height = dimensions.height; 
     }
@@ -10,13 +12,26 @@
       context.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
     update(){
-      
+
+      if (this.position.y + this.height > canvas.height){
+        this.velocity.y = canvas.height - (this.position.y + this.height)
+      } else {
+        this.velocity.y += gravity
+      }
+      this.position.x += this.velocity.x
+      this.position.y += this.velocity.y 
+
+      this.draw()
     }
  }
  const bonequinha = new Sprite({
     position: {
       x: 100, 
       y: 100
+    },
+    velocity: {
+      x: 0,
+      y: 2
     },
     dimensions: {
       width: 50,
