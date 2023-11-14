83a3c8815a2290dd1211c5cff72b15d1e9fcff96
Estelita Fernanda <112116442+estelitafernanda@users.noreply.github.com>
2023-11-13 22:54:43 -0300

    Update script.js

--- jogo/script.js
+++ jogo/script.js
@@ -1,12 +1,22 @@
 let canvas = document.getElementById('canvas');
 let context = canvas.getContext('2d');
+
 let imagem = new Image();
+
 imagem.src = "bonequinha.png";
 
 let x = 0, intervalo = 120, y = 0, movi = 0;
+let pulando = false;
+let alturaInicialDoSalto = 0;
+let alturaMaximaDoSalto = 100;
+let velocidadeDoSalto = 5;
+let gravidade = 5;
 
-/*document.addEventListener('keydown', function (event) {
+let teclasPressionadas = {};
+
+document.addEventListener('keydown', function (event) {
     var key = event.key.toLowerCase();
+    teclasPressionadas[key] = true;
 
     if (key === 's') {
         y += 10;
@@ -14,86 +24,99 @@ let x = 0, intervalo = 120, y = 0, movi = 0;
     } else if (key === 'a') {
         x -= 10;
         esquerda();
-    } else if (key === 'w') {
-        y -= 10;
-        cima();
+    } else if (key === 'w' && !pulando) {
+        pulando = true;
+        alturaInicialDoSalto = y;
+        animar(); 
     } else if (key === 'd') {
         x += 10;
         direita();
     }
-});*/
+
+});
+document.addEventListener('keyup', function (event) {
+    var key = event.key.toLowerCase();
+    teclasPressionadas[key] = false;
+});
 
 function baixo() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        //if (x === 0) {
-            context.drawImage(imagem, 44, -30, 90, 100, 0, 0, 160, 160);
-        //}
-        //else if (x === 1) {
-            context.drawImage(imagem, 110, -30, 90, 100, 0, 0, 160, 160);
-    // }
-        //else if (x === 2) {
-            context.drawImage(imagem, 176, -30, 90, 100, 0, 0, 160, 160);
-        //}
-        //else if (x === 3) {
-            context.drawImage(imagem, 242, -30, 90, 100, 0, 0, 160, 160);
-        //}
-        //movi = (x + 1) % 4;
-    //}
-    //imagem.onload = baixo;
-
-    /*function esquerda() {
-        context.clearRect(0, 0, canvas.width, canvas.height);*/
-        //if (x === 0) {
-            context.drawImage(imagem, 45, 64, 90, 70, 3, 40, 157, 120);
-        //}
-        //else if (x === 1) {
-            context.drawImage(imagem, 110, 64, 90, 70, 1, 43, 157, 120);
-        //}
-        //else if (x === 2) {
-            context.drawImage(imagem, 175, 65, 90, 70, 0, 42, 157, 120);
-        //}
-        //else if (x === 3) {
-            context.drawImage(imagem, 241, 65, 90, 70, 0, 45, 157, 120);
-    /*  }
-        movi = (movi + 1) % 4;
-    }*/
-
-    //imagem.onload = esquerda;
-
-    /*function direita() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        if (y === 0) {*/
-            context.drawImage(imagem, 40, 130, 90, 70, 0, 41, 146, 120);
-    /* }
-        else if (y === 1) {*/
-            context.drawImage(imagem, 106, 131, 90, 70, 0, 46, 146, 120);
-        /*}
-        else if (y === 2) {*/
-            context.drawImage(imagem, 170, 131, 90, 70, -3, 43, 146, 120);
-        /*}
-        else if (y === 3) {*/
-            context.drawImage(imagem, 236, 131, 90, 70, -3, 47, 146, 120);
-    /* }
-        movi = (movi + 1) % 4;*/
-    //}
-
-    //imagem.onload = direita;
-    /*function cima() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        if (x === 0) {*/
-            context.drawImage(imagem, 36, 200, 90, 70, -6, 57, 146, 110);
-        /*}
-        else if (x === 1) {*/
-            context.drawImage(imagem, 101, 200, 90, 70, -6, 59, 146, 110);
-        /*}
-        else if (x === 2) {*/
-            context.drawImage(imagem, 168, 200, 90, 70, -5, 58, 146, 110);
-        /*}
-        else if (x === 3) {*/
-            context.drawImage(imagem, 235, 200, 90, 70, -5, 59, 146, 110);
-        /*
-        movi = (movi + 1) % 4;*/
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (movi === 0) {
+        context.drawImage(imagem, 44, -30, 90, 100, x, y, 160, 160);
+    }
+    else if (movi === 1) {
+        context.drawImage(imagem, 110, -30, 90, 100, x, y, 160, 160);
+    }
+    else if (movi === 2) {
+        context.drawImage(imagem, 176, -30, 90, 100, x, y, 160, 160);
+    }
+    else if (movi === 3) {
+        context.drawImage(imagem, 242, -30, 90, 100, x, y, 160, 160);
+    }
+    movi = (movi + 1) % 4;
+}
+function esquerda() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (movi === 0) {
+        context.drawImage(imagem, 45, 64, 90, 70, x + 3, y + 40, 157, 120);
+    }
+    else if (movi === 1) {
+        context.drawImage(imagem, 110, 64, 90, 70, x + 1, y + 43, 157, 120);
+    }
+    else if (movi === 2) {
+        context.drawImage(imagem, 175, 65, 90, 70, x, y + 42, 157, 120);
+    }
+    else if (movi === 3) {
+        context.drawImage(imagem, 241, 65, 90, 70, x, y + 45, 157, 120);
+    }
+    movi = (movi + 1) % 4;
 }
 
-imagem.onload = baixo;
 
+function direita() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (movi === 0) {
+        context.drawImage(imagem, 40, 130, 90, 70, x, y + 41, 146, 120);
+    }
+    else if (movi === 1) {
+        context.drawImage(imagem, 106, 131, 90, 70, x, y + 46, 146, 120);
+    }
+    else if (movi === 2) {
+        context.drawImage(imagem, 170, 131, 90, 70, x -3, y + 43, 146, 120);
+    }
+    else if (movi === 3) {
+        context.drawImage(imagem, 236, 131, 90, 70, x -3, y + 47, 146, 120);
+    }
+    movi = (movi + 1) % 4;
+}
+
+function animar() {
+    if (pulando) {
+        // Atualize a posição vertical durante o pulo
+        y -= velocidadeDoSalto;
+
+        // Verifique se atingiu a altura máxima do pulo
+        if (y <= alturaInicialDoSalto - alturaMaximaDoSalto) {
+            pulando = false;
+            // Resetar a posição ao atingir a altura máxima do pulo
+            y = canvas.height - 160;
+        } else {
+            requestAnimationFrame(animar);
+        }
+        
+    } else {
+        // Aplica a gravidade para simular a queda
+        if (y < canvas.height - 160) { // Ajuste conforme necessário
+            y += gravidade;
+        }
+
+        // Volta para o ponto inicial se a tecla 'w' não estiver pressionada
+        if (!teclasPressionadas['w'] && y < canvas.height - 160) {
+            y += gravidade;
+        }
+
+        requestAnimationFrame(animar);
+    }
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(imagem, 44, -30, 90, 100, x, y, 160, 160);
+}
